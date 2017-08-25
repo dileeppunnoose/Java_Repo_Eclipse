5a8b96679fd183f7369ec2e51b6f75acb951af7f
dileeppunnoose <31214906+dileeppunnoose@users.noreply.github.com>
2017-08-25 09:32:06 -0600

    Initial commit

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,19 @@
+
+public class Hello {
+	public static void main(String[] args){
+        System.out.println("Hello World - Dileep");
+        int myFirstNumber = (10+3) + (2*3);
+        int mySecondNumber = 12;
+        int myThirdNumber = mySecondNumber*10;
+        int myTotal= myFirstNumber+mySecondNumber+myThirdNumber;
+        int challengeQuestion= 1000-myTotal;
+        System.out.println(myTotal);
+        System.out.println(challengeQuestion);
+
+        int a=10;
+        byte b=11;
+        short c=12;
+        long d=50000*(a+b+c);
+        System.out.println(d);
+    }
+}
